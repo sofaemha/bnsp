fb6239825e7d0e356695d764c689198ebee26dad
sofaemha <mail@sofa.my.id>
2026-02-11 20:43:18 +0700

    feat: Add new v2 authentication layout and introduce application-wide configuration.

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -3,12 +3,12 @@ import packageJson from "../../package.json";
 const currentYear = new Date().getFullYear();
 
 export const APP_CONFIG = {
-  name: "BNSP",
+  name: "PT Maju Jaya",
   version: packageJson.version,
-  copyright: `© ${currentYear}, BNSP.`,
+  copyright: `© ${currentYear}, PT Maju Jaya.`,
   meta: {
-    title: "BNSP - Modern Next.js Dashboard Starter Template",
+    title: "PT Maju Jaya - Modern Next.js Dashboard Starter Template",
     description:
-      "BNSP is a modern, open-source dashboard starter template built with Next.js 16, Tailwind CSS v4, and shadcn/ui. Perfect for SaaS apps, admin panels, and internal tools—fully customizable and production-ready.",
+      "PT Maju Jaya is a modern, open-source dashboard starter template built with Next.js 16, Tailwind CSS v4, and shadcn/ui. Perfect for SaaS apps, admin panels, and internal tools—fully customizable and production-ready.",
   },
 };
